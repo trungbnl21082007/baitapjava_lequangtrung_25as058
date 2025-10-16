e5328ee57a9861a7096452770ba6e456c35e110f
trung <trungbnl21082007@gmail.com>
2025-10-16 23:29:45 +0700

    Bai tap chuong 2

---
+++ src/InvoiceItem.java
@@ -0,0 +1,48 @@
+public class InvoiceItem {
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceItem(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    // Các phương thức getter (truy xuất thông tin)
+    public String getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    // Các phương thức setter (cập nhật thông tin)
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotal() {
+        return unitPrice * qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem[id=" + id + ",desc=" + desc + ",qty=" + qty + ",unitPrice=" + unitPrice + "]";
+    }
+}
